d51a5ff635c50f2ce03f35a4a26bd4a1a48d9d21
edwinkmusaasizi <edwinkmusaasizi@gmail.com>
2025-09-02 22:38:56 +0300

    update of sevice templates

--- services/forms.py
+++ services/forms.py
@@ -1,8 +1,14 @@
 # services/forms.py
 from django import forms
-from .models import Service  # This imports your Service model
+from .models import Service
 
 class ServiceForm(forms.ModelForm):
     class Meta:
         model = Service
         fields = ['facility', 'name', 'description', 'category', 'skill_type']
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Add Bootstrap form-control class to all form fields
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
